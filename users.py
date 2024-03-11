4cf752502c90a525e88db8a0e968667f951b15ee
mikemtzp <mikemp8226@gmail.com>
2024-03-10 18:21:56 -0600

    add get endpoints for users

--- users.py
+++ users.py
@@ -1,8 +1,68 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get("/users")
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    url: str
+    age: int
+
+
+users_list = [
+    User(id=1, name="Patrick", surname="Galante", url="https://i9k.dev", age=32),
+    User(
+        id=2, name="Courtney", surname="LaPlante", url="https://spiritbox.com", age=35
+    ),
+    User(id=3, name="Brian", surname="Garris", url="https://kloose.com", age=30),
+]
+
+
+@app.get("/usersjson")
 async def get_users():
-    return "Hello Users!"
+    return [
+        {
+            "name": "Patrick",
+            "surname": "Galante",
+            "url": "https://i9k.dev",
+            "age": 32,
+        },
+        {
+            "name": "Courtney",
+            "surname": "LaPlante",
+            "url": "https://spiritbox.com",
+            "age": 35,
+        },
+        {
+            "name": "Brian",
+            "surname": "Garris",
+            "url": "https://kloose.com",
+            "age": 30,
+        },
+    ]
+
+
+@app.get("/users")
+async def users():
+    return users_list
+
+
+@app.get("/user/{id}")  # Path
+async def user(id: int):
+    return search_user(id)
+
+
+@app.get("/user/")  # Query
+async def user(id: int):
+    return search_user(id)
+
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "User not found"}
