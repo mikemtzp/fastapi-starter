be4c0badb5197effe2db07a95f9679f79d6b666b
mikemtzp <mikemp8226@gmail.com>
2024-03-13 16:15:18 -0600

    add oauth2 aunthentication

--- routers/auth_users.py
+++ routers/auth_users.py
@@ -1,7 +1,10 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
 
 app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 
 class User(BaseModel):
@@ -32,6 +35,48 @@ users_db = {
     },
 }
 
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username])
+
+
 def search_user(username: str):
     if username in users_db:
-        return UserDB(users_db[username])
+        return User(**users_db[username])
+
+
+async def current_user(token: str = Depends(oauth2)):
+    user = search_user(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Unactive user",
+        )
+
+    return user
+
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(status_code=400, detail="Invalid user")
+
+    user = search_user_db(form.username)
+    if not form.password == user.password:
+        raise HTTPException(status_code=400, detail="Wrong password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
